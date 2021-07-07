ca3a40a664e61b46192d1221e4bd065109053f0d
yunmi <heoyunme0509@naver.com>
2021-07-07 13:15:13 +0900

    음성인식 구현

---
+++ app/src/main/java/com/example/bears/SpeechToText.java
@@ -0,0 +1,92 @@
+package com.example.bears;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SpeechToText implements RecognitionListener {
+    Context context;
+
+    public SpeechToText(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+        Toast.makeText(context, "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+    }
+
+    @Override
+    public void onError(int error) {
+        String message;
+        switch (error) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "오디오 에러";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "클라이언트 에러";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "퍼미션 없음";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "네트워크 에러";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "네트웍 타임아웃";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "찾을 수 없음";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RECOGNIZER가 바쁨";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "서버가 이상함";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "말하는 시간초과";
+                break;
+            default:
+                message = "알 수 없는 오류임";
+                break;
+        }
+        Log.d("음성인식 에러메시지", message);
+    }
+
+    @Override
+    public void onResults(Bundle results) { // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        Log.d("ArrayList", matches.toString());
+        Toast.makeText(context, "음성인식 결과 : " + matches.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+    }
+}
