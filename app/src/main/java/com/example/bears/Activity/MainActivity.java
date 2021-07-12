09f27c181042b9b3d10a2a533c7ebf6a14fba31c
yunmi <heoyunme0509@naver.com>
2021-07-12 15:14:38 +0900

    gps 구현완료

--- app/src/main/java/com/example/bears/Activity/MainActivity.java
+++ app/src/main/java/com/example/bears/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bears.Activity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -13,11 +15,20 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.example.bears.R;
 import com.example.bears.SpeechToText;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
@@ -27,26 +38,36 @@ public class MainActivity extends AppCompatActivity {
     EditText et_searchnum;
     int pressedTime = 0;
     final int PERMISSION = 1;
+    private FusedLocationProviderClient fusedLocationClient;
+    public Location location;
+    String tmX,tmY;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (Build.VERSION.SDK_INT >= 23) { // 퍼미션 체크
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO}, PERMISSION);
-        }
-
         ll_voice = findViewById(R.id.ll_voice);
         ll_bookmark = findViewById(R.id.ll_bookmark);
         ll_driver = findViewById(R.id.ll_driver);
 
         iv_searchbtn = findViewById(R.id.iv_searchbtn);
         et_searchnum = findViewById(R.id.et_searchnum);
-;
+        ;
         intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        //권한체크
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET,Manifest.permission.RECORD_AUDIO})
+                .check();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        performAction();
+
 
         ll_voice.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,23 +107,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            performAction();
+        }
 
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+            Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
     private SpeechToText listener = new SpeechToText(this);
+
+    @SuppressLint("MissingPermission")
+    private void performAction() {
+        fusedLocationClient.getLastLocation()
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("실패이유", "PerformAction", e.getCause());
+                    }
+                })
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        Log.d("위치", "performAction " + location);
+
+                        if (location != null) {
+                            // Logic to handle location object
+                            Log.d("위도", "getLatitude: " + location.getLatitude());
+                            tmY=String.valueOf(location.getLatitude());
+                            Log.d("경도", "getLongitude: " + location.getLongitude());
+                            tmX=String.valueOf(location.getLongitude());
+
+
+                        }
+                    }
+                });
+    }
 
     @Override
     public void onBackPressed() {
-        if ( pressedTime == 0 ) {
-            Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다." , Toast.LENGTH_LONG).show();
+        if (pressedTime == 0) {
+            Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
             pressedTime = (int) System.currentTimeMillis();
-        }
-        else {
+        } else {
             int seconds = (int) (System.currentTimeMillis() - pressedTime);
 
-            if ( seconds > 2000 ) {
-                Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다." , Toast.LENGTH_LONG).show();
-                pressedTime = 0 ;
-            }
-            else {
+            if (seconds > 2000) {
+                Toast.makeText(MainActivity.this, " 한 번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
+                pressedTime = 0;
+            } else {
                 super.onBackPressed();
 //                finish(); // app 종료 시키기
             }
