0f62dfd6039ab49092cd677d68d8dad1e7aaa857
yunmi <heoyunme0509@naver.com>
2021-08-01 17:12:25 +0900

    버스 도착시간 countDown 구현중

--- app/src/main/java/com/example/bears/Activity/SearchResultActivity.java
+++ app/src/main/java/com/example/bears/Activity/SearchResultActivity.java
@@ -2,6 +2,7 @@ package com.example.bears.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
@@ -22,10 +23,10 @@ public class SearchResultActivity extends AppCompatActivity {
     ImageView iv_backbtn, iv_star;
     TextView tv_busnum, tv_arrvaltime, tv_arrivalbusstop;
     int i = 0;
-    static String busnumber, ars_Id, stationNm;
-    String stationByUidUrl, BusStopServiceKey;
+    static String busnumber, ars_Id, stationNm,result;
+    String stationByUidUrl, BusStopServiceKey,seconds,minutes,corrent_result;
     public HashMap<String, String> StationByResultMap;
-
+    String [] array;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +37,7 @@ public class SearchResultActivity extends AppCompatActivity {
         ars_Id = intent.getStringExtra("ars_Id");
         stationNm = intent.getStringExtra("stationNm");
         BusStopServiceKey = "SPJi5n0Hw%2Fbd8BBVjSB1hS8hnWIi95BW8oRu%2BN9lFGt%2Bpqu6gfnEPwYfXuOMsJ8ko8nJ1A1EWDOs1oNPommygQ%3D%3D";
-
+        result =null;
         ll_bookmark = findViewById(R.id.ll_bookmark);
         ll_bell = findViewById(R.id.ll_bell);
         iv_backbtn = findViewById(R.id.iv_backbtn);
@@ -83,8 +84,29 @@ public class SearchResultActivity extends AppCompatActivity {
                                     StationByUidItem stationByUidItem = new StationByUidItem(stationByUidUrl, busnumber);
                                     stationByUidItem.execute();
                                     StationByResultMap = stationByUidItem.get();
-                                    Log.d("StationByUid 결과", "arrmsg1 : " + StationByResultMap.get("arrmsg1"));
-                                    tv_arrvaltime.setText(StationByResultMap.get("arrmsg1"));
+                                    corrent_result = StationByResultMap.get("arrmsg1");
+                                    if(!corrent_result.equals(result)) {
+                                        result = corrent_result;
+                                        Log.d("StationByUid 결과", "arrmsg1 : " + result);
+                                        try {
+                                            array = result.split("\\[");
+                                            minutes = array[0].substring(0, result.indexOf("분"));
+                                            seconds = array[0].substring(result.indexOf("분") + 1, result.indexOf("초"));
+                                            countDown();
+//                                        tv_arrvaltime.setText(minutes+"분 "+seconds+"초");
+                                        } catch (Exception e) {
+                                            tv_arrvaltime.setText(result);
+                                        }
+                                        if (result != "곧도착") {
+                                            String result2 = array[1].substring(0, array[1].length() - 1);
+                                            tv_arrivalbusstop.setText(result2);
+                                        }
+                                    }
+
+
+
+
+
                                 } catch (ExecutionException e) {
                                     e.printStackTrace();
                                 } catch (InterruptedException e) {
@@ -131,5 +153,61 @@ public class SearchResultActivity extends AppCompatActivity {
         });
 
 
+    }
+    public void countDown() {
+
+        long conversionTime = 0;
+
+        // 1000 단위가 1초
+        // 60000 단위가 1분
+        // 60000 * 3600 = 1시간
+
+        // 변환시간
+        conversionTime = Long.valueOf(minutes) * 60 * 1000 + Long.valueOf(seconds) * 1000;
+
+        // 첫번쨰 인자 : 원하는 시간 (예를들어 30초면 30 x 1000(주기))
+        // 두번쨰 인자 : 주기( 1000 = 1초)
+        new CountDownTimer(conversionTime, 1000) {
+
+            // 특정 시간마다 뷰 변경
+            public void onTick(long millisUntilFinished) {
+
+//                // 시간단위
+//                String hour = String.valueOf(millisUntilFinished / (60 * 60 * 1000));
+
+                // 분단위
+                long getMin = millisUntilFinished - (millisUntilFinished / (60 * 60 * 1000));
+                String min = String.valueOf(getMin / (60 * 1000)); // 몫
+
+                // 초단위
+                String second = String.valueOf((getMin % (60 * 1000)) / 1000); // 나머지
+
+//                // 밀리세컨드 단위
+//                String millis = String.valueOf((getMin % (60 * 1000)) % 1000); // 몫
+
+//                // 시간이 한자리면 0을 붙인다
+//                if (hour.length() == 1) {
+//                    hour = "0" + hour;
+//                }
+//
+//                // 분이 한자리면 0을 붙인다
+//                if (min.length() == 1) {
+//                    min = "0" + min;
+//                }
+
+                tv_arrvaltime.setText(min+"분 "+second+"초");
+//                count_view.setText(hour + ":" + min + ":" + second);
+            }
+
+            // 제한시간 종료시
+            public void onFinish() {
+
+                // 변경 후
+//                count_view.setText("촬영종료!");
+
+                // TODO : 타이머가 모두 종료될때 어떤 이벤트를 진행할지
+
+            }
+        }.start();
     }
 }
